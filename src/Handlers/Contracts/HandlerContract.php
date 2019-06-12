20c2d1473d54a2106fced7c705bece8aa373afbe
simon <simon@crcms.cn>
2019-06-12 14:28:07 +0800

    Remove single interface mandatory constraints

--- src/Handlers/Contracts/HandlerContract.php
+++ src/Handlers/Contracts/HandlerContract.php
@@ -2,13 +2,6 @@
 
 namespace CrCms\Foundation\Handlers\Contracts;
 
-use CrCms\Foundation\Transporters\Contracts\DataProviderContract;
-
 interface HandlerContract
 {
-    /**
-     * @param DataProviderContract $provider
-     * @return mixed
-     */
-    public function handle(DataProviderContract $provider);
 }
--- src/Tasks/Contracts/TaskContract.php
+++ src/Tasks/Contracts/TaskContract.php
@@ -4,9 +4,4 @@ namespace CrCms\Foundation\Tasks\Contracts;
 
 interface TaskContract
 {
-    /**
-     * @param mixed ...$params
-     * @return mixed
-     */
-    public function handle(...$params);
 }
