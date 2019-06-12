fad222ce6464545d8e51e16dbbef1ad6b6009903
simon <simon@crcms.cn>
2019-06-12 14:26:34 +0800

    Increase enum value for multiple speeds

---
+++ tests/Enums/DemoEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace CrCms\Foundation\Tests\Enums;
+
+use CrCms\Foundation\Enums\AbstractEnum;
+
+class DemoEnum extends AbstractEnum
+{
+    protected const TEST = 1;
+}
---
+++ tests/Enums/EnumTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CrCms\Foundation\Tests\Enums;
+
+use PHPUnit\Framework\TestCase;
+
+class EnumTest extends TestCase
+{
+    public function testEnum()
+    {
+        $this->assertEquals(DemoEnum::TEST()->getValue(),1);
+        $this->assertEquals(DemoEnum::TEST()->getKey(),'TEST');
+        $this->assertEquals(DemoEnum::TEST_VALUE(),1);
+        $this->assertEquals(DemoEnum::TEST_KEY(),'TEST');
+    }
+}
