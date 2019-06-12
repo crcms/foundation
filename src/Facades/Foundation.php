18ffd1bb5e2b8f4c62daf223dd6fa1ad56c8d765
simon <simon@crcms.cn>
2019-06-12 15:11:22 +0800

    Framework merge

---
+++ src/Facades/Foundation.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CrCms\Foundation\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static bool isLumen()
+ * @method static bool isLaravel()
+ */
+class Foundation extends Facade
+{
+    /**
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return \CrCms\Foundation\Foundation\Foundation::class;
+    }
+}
---
+++ src/Foundation/Foundation.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace CrCms\Foundation\Foundation;
+
+use Illuminate\Contracts\Container\Container;
+
+class Foundation
+{
+    /**
+     * @var Container
+     */
+    protected $container;
+
+    /**
+     * @param Container $container
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     *
+     * @return bool
+     */
+    public function isLumen(): bool
+    {
+        return $this->container instanceof \Laravel\Lumen\Application;
+    }
+
+    /**
+     *
+     * @return bool
+     */
+    public function isLaravel(): bool
+    {
+        return $this->container instanceof \Illuminate\Foundation\Application;
+    }
+}
