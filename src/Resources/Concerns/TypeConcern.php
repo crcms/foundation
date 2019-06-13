ac09844017ec690603e7849a772003562d114bde
simon <simon@crcms.cn>
2019-06-13 10:38:19 +0800

    Add numericCastString

--- src/Resources/Concerns/TypeConcern.php
+++ src/Resources/Concerns/TypeConcern.php
@@ -10,6 +10,30 @@ trait TypeConcern
     protected $casts = [];
 
     /**
+     * @var bool
+     */
+    protected $numericCastString = true;
+
+    /**
+     * @return bool
+     */
+    public function getNumericCastString(): bool
+    {
+        return $this->numericCastString;
+    }
+
+    /**
+     * @param bool $numericCastString
+     *
+     * @return TypeConcern
+     */
+    public function setNumericCastString(bool $numericCastString): self
+    {
+        $this->numericCastString = $numericCastString;
+        return $this;
+    }
+
+    /**
      * @param $value
      * @param string $field
      *
@@ -28,6 +52,10 @@ trait TypeConcern
      */
     protected function cast(string $type, $value)
     {
+        if ($this->numericCastString && is_numeric($value)) {
+            return strval($value);
+        }
+
         switch ($type) {
             case 'int':
             case 'integer':
