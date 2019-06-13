82b676d5a644ccf637b5b37654b7ff1f632a86e5
simon <simon@crcms.cn>
2019-06-13 09:48:22 +0800

    include WhenMakeConcern

--- src/Resources/ResourceCollection.php
+++ src/Resources/ResourceCollection.php
@@ -2,6 +2,7 @@
 
 namespace CrCms\Foundation\Resources;
 
+use CrCms\Foundation\Resources\Concerns\WhenMakeConcern;
 use CrCms\Foundation\Resources\Concerns\SceneConcern;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection as BaseResourceCollection;
@@ -11,7 +12,7 @@ use Illuminate\Http\Resources\Json\ResourceCollection as BaseResourceCollection;
  */
 class ResourceCollection extends BaseResourceCollection
 {
-    use SceneConcern;
+    use SceneConcern, WhenMakeConcern;
 
     /**
      * ResourceCollection constructor.
@@ -31,5 +32,14 @@ class ResourceCollection extends BaseResourceCollection
     public function toArray($request): array
     {
         return $this->collection->map->scene($this->scene)->fields($this->fields)->toArray($request)->all();
+    }
+
+    /**
+     *
+     * @return array
+     */
+    protected static function emptyReturn(): array
+    {
+        return [];
     }
 }
